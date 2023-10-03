2caaf983a9ca9b66b2c4cd90a937b2eb64db9ff6
yuta mochizuki <yutamoty@gmail.com>
2023-10-03 14:17:21 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ddns-cloudflare
+============
+
+This script deliver dynamic DNS in Cloudflare.
---
+++ update_record.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -xe
+#
+# This script deliver dynamic DNS in Cloudflare.
+
+# Please edit the following config file for your environment.
+CURRENT=$(cd $(dirname $0);pwd)
+CONFIG_FILE=${1:-${CURRENT}/.ddns-cloudflare.conf}
+
+if [ ! -r "${CONFIG_FILE}" ]; then
+    echo "${CONFIG_FILE} does not exist or unreadable." 1>&2
+    exit 1
+fi
+
+source $CONFIG_FILE
+
+# Check required commands.
+curl --request PATCH \
+  --url https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${RECORD_ID} \
+  --header 'Content-Type: application/json' \
+  --header "Authorization: Bearer ${API_TOKEN}" \
+  --header "X-Auth-Email: ${AUTH_EMAIL}" \
+  --data '{
+  "content": "'"${NEW_RR_VALUE}"'",
+  "name": "'"${HOST_NAME}"'",
+  "type": "A",
+  "comment": "Domain verification record"
+}'
+
+if [ $? -eq 0 ]; then
+    $LOGGER "UPSERT: ${HOST_NAME}, ${NEW_RR_VALUE}"
+fi
